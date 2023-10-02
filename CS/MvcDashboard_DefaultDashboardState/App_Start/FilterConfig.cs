bb34905b717ae2e00c1160d04dcd041783e6b065
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 18:01:08 +0400

    Change example status to out of maintenance

--- CS/MvcDashboard_DefaultDashboardState/App_Start/FilterConfig.cs
+++
@@ -1,9 +0,0 @@
-using System.Web.Mvc;
-
-namespace MvcDashboard_DefaultDashboardState {
-    public class FilterConfig {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
-            filters.Add(new HandleErrorAttribute());
-        }
-    }
-}
--- CS/MvcDashboard_DefaultDashboardState/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Codebehind="Global.asax.cs" Inherits="MvcDashboard_DefaultDashboardState.MvcApplication" Language="C#" %>
